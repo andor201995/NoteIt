a65e4a8c0c946c3cb7186af7b2272f182d49d9b6
Anmol Srivastava <andor201995@users.noreply.github.com>
2019-05-26 18:21:17 +0530

    feature:adding Title and cleaning up code

--- app/src/main/java/com/andor/navigate/notepad/listing/AddNoteFragment.kt
+++ app/src/main/java/com/andor/navigate/notepad/listing/AddNoteFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.andor.navigate.notepad.R
@@ -13,21 +12,24 @@ import kotlinx.android.synthetic.main.fragment_add_note.*
 
 class AddNoteFragment : Fragment() {
 
-    private lateinit var noteRepo: NoteRepo
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        noteRepo = NoteRepoImpl
         return inflater.inflate(R.layout.fragment_add_note, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         submitNote.setOnClickListener { view ->
-            noteRepo.addNote(ListModel("Default Head", newNoteBodyTxt.text.toString()))
+            NoteRepoImpl.addNote(
+                ListModel(
+                    AddNoteFragmentArgs.fromBundle(arguments!!).headText,
+                    newNoteBodyTxt.text.toString()
+                )
+            )
             Navigation.findNavController(view).navigateUp()
         }
     }
--- app/src/main/java/com/andor/navigate/notepad/listing/NoteListingFragment.kt
+++ app/src/main/java/com/andor/navigate/notepad/listing/NoteListingFragment.kt
@@ -1,55 +1,76 @@
 package com.andor.navigate.notepad.listing
 
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.andor.navigate.notepad.MainActivity
 import com.andor.navigate.notepad.R
 import kotlinx.android.synthetic.main.fragment_note_listing.*
 
 
 class NoteListingFragment : Fragment() {
 
-    private lateinit var noteRepo: NoteRepo
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        noteRepo = NoteRepoImpl
         return inflater.inflate(R.layout.fragment_note_listing, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         hideKeyBoard()
-        setSubmitClickEvent()
+        setAddNoteClickEvent()
         setUpListAdapter()
     }
 
-    private fun setSubmitClickEvent() {
-        addNoteButton.setOnClickListener { view ->
-            Navigation.findNavController(view).navigate(R.id.action_noteListingFragment_to_addNoteFragment)
+    private fun setAddNoteClickEvent() {
+        addNoteButton.setOnClickListener {
+            val dialog = Dialog(context!!)
+            dialog.setContentView(R.layout.new_note_dialog)
+            dialog.setTitle(R.string.new_note_hint)
+
+            val newNoteImageButtonAccpt = dialog.findViewById<ImageButton>(R.id.newNoteButtonAccept)
+            val newNoteImageButtonReject = dialog.findViewById<ImageButton>(R.id.newNoteButtonCancel)
+
+            newNoteImageButtonAccpt.setOnClickListener {
+                val newHeadText = dialog.findViewById<EditText>(R.id.newNoteHeadText).text.toString()
+
+                val action = if (newHeadText.isBlank()) {
+                    NoteListingFragmentDirections.actionNoteListingFragmentToAddNoteFragment()
+                } else {
+                    NoteListingFragmentDirections.actionNoteListingFragmentToAddNoteFragment(newHeadText)
+                }
+                dialog.cancel()
+                Navigation.findNavController(view!!).navigate(action)
+            }
+            newNoteImageButtonReject.setOnClickListener {
+                dialog.cancel()
+            }
+            dialog.show()
         }
     }
 
     private fun hideKeyBoard() {
         val imm = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(getView()!!.windowToken, 0)
+        imm.hideSoftInputFromWindow(view!!.windowToken, 0)
     }
 
     private fun setUpListAdapter() {
-        val contentList = noteRepo.getNotes()
+        val contentList = NoteRepoImpl.getNotes()
         val listingAdapter = ListingAdapter(context!!, contentList)
         listRecyclerView.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
         val linearLayoutManager = LinearLayoutManager(context)
