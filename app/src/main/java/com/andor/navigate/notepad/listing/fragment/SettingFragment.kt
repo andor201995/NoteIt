f4b197e5e8d1a9011bccad6b1b8ac95f77529aa2
Anmol Srivastava <andor201995@users.noreply.github.com>
2019-06-06 16:40:23 +0530

    Setting up the type's of list

--- app/src/main/java/com/andor/navigate/notepad/listing/fragment/SettingFragment.kt
+++ app/src/main/java/com/andor/navigate/notepad/listing/fragment/SettingFragment.kt
@@ -1,14 +1,18 @@
 package com.andor.navigate.notepad.listing.fragment
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.andor.navigate.notepad.R
 import com.andor.navigate.notepad.auth.AuthActivity
 import com.andor.navigate.notepad.auth.UserAuthentication
+import com.andor.navigate.notepad.core.ListingType
 import com.andor.navigate.notepad.core.NoteViewModel
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : Fragment() {
 
@@ -26,6 +30,50 @@ class SettingFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(activity!!).get(NoteViewModel::class.java)
+
+        //init setting background
+        viewModel.appStateRelay.value!!.let {
+            when (it.listingType) {
+                is ListingType.Linear -> {
+                    setting_view_type_linear.background =
+                        ContextCompat.getDrawable(context!!, R.drawable.border)
+                }
+                is ListingType.Grid -> {
+                    setting_view_type_grid.background =
+                        ContextCompat.getDrawable(context!!, R.drawable.border)
+                }
+                is ListingType.Stagered -> {
+                    setting_view_type_staggered.background =
+                        ContextCompat.getDrawable(context!!, R.drawable.border)
+                }
+            }
+        }
+        setUpOnClickListType()
+
+    }
+
+    private fun setUpOnClickListType() {
+        setting_view_type_linear.setOnClickListener {
+            viewModel.appStateRelay.postValue(viewModel.appStateRelay.value!!.copy(listingType = ListingType.Linear))
+            clearButtonBackGround()
+            it.background = ContextCompat.getDrawable(context!!, R.drawable.border)
+        }
+        setting_view_type_grid.setOnClickListener {
+            viewModel.appStateRelay.postValue(viewModel.appStateRelay.value!!.copy(listingType = ListingType.Grid))
+            clearButtonBackGround()
+            it.background = ContextCompat.getDrawable(context!!, R.drawable.border)
+        }
+        setting_view_type_staggered.setOnClickListener {
+            viewModel.appStateRelay.postValue(viewModel.appStateRelay.value!!.copy(listingType = ListingType.Stagered))
+            clearButtonBackGround()
+            it.background = ContextCompat.getDrawable(context!!, R.drawable.border)
+        }
+    }
+
+    private fun clearButtonBackGround() {
+        setting_view_type_linear.setBackgroundColor(Color.WHITE)
+        setting_view_type_grid.setBackgroundColor(Color.WHITE)
+        setting_view_type_staggered.setBackgroundColor(Color.WHITE)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
