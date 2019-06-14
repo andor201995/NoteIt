14e80d9a060d1186865e5a6ce64ddcbebf351636
Anmol Srivastava <andor201995@users.noreply.github.com>
2019-06-14 13:20:56 +0530

    HK

--- app/src/main/java/com/andor/navigate/notepad/listing/fragment/BottomSheetMenuFragment.kt
+++ app/src/main/java/com/andor/navigate/notepad/listing/fragment/BottomSheetMenuFragment.kt
@@ -6,13 +6,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.FrameLayout
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.andor.navigate.demonavigation.fragment.BottomSheetNavFragment
 import com.andor.navigate.notepad.R
 import com.andor.navigate.notepad.core.NoteViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
 
 open class BottomSheetMenuFragment : BottomSheetDialogFragment() {
 
@@ -33,6 +37,7 @@ open class BottomSheetMenuFragment : BottomSheetDialogFragment() {
         childFragmentManager.beginTransaction()
             .replace(R.id.bottomMenuContainer, bottomSheetNavFragment, "Bottom_sheet_Fragment")
             .addToBackStack("Bottom_sheet_Fragment").commit()
+
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -52,5 +57,14 @@ open class BottomSheetMenuFragment : BottomSheetDialogFragment() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
 
+        dialog?.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        val bottomSheet = dialog.findViewById(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+
+    }
 }
