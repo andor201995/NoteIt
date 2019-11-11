98db7abe147b2225341d083a285850efb93dff84
anmol-5732 <anmol.s@zohocorp.com>
2019-11-12 00:44:30 +0530

    ViewPager2 in Expanded Note

--- app/src/main/java/com/andor/navigate/notepad/expanded/ExpandedNoteFragment.kt
+++ app/src/main/java/com/andor/navigate/notepad/expanded/ExpandedNoteFragment.kt
@@ -2,16 +2,14 @@ package com.andor.navigate.notepad.expanded
 
 
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.andor.navigate.notepad.R
-import com.andor.navigate.notepad.core.AppState
 import com.andor.navigate.notepad.core.NoteViewModel
-import com.andor.navigate.notepad.core.Utils
 import com.andor.navigate.notepad.core.navigateSafe
 import com.andor.navigate.notepad.listing.NotesActivity
 import kotlinx.android.synthetic.main.fragment_expanded_note.*
@@ -19,7 +17,6 @@ import kotlinx.android.synthetic.main.fragment_expanded_note.*
 
 class ExpandedNoteFragment : Fragment() {
 
-    private lateinit var oldAppState: AppState
     private lateinit var viewModel: NoteViewModel
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -27,31 +24,33 @@ class ExpandedNoteFragment : Fragment() {
 
         viewModel = ViewModelProvider(activity!!).get(NoteViewModel::class.java)
 
-        expandedNoteTxt.movementMethod = ScrollingMovementMethod()
-        setDoubleTapListener()
+        val viewPagerAdapter = ViewPagerAdapter(context!!) {
+            if (it is ViewPageItemEvent.DoubleClickEvent) {
+                if (it.adapterPosition >= 0) {
+                    openEditor()
+                }
+            }
+        }
+        val value = viewModel.getAppStateStream().value!!
+        val listOfAllNotes = value.listOfAllNotes
+        viewPagerAdapter.pageList = listOfAllNotes
+        expanded_view_pager.adapter = viewPagerAdapter
+        expanded_view_pager.setCurrentItem(listOfAllNotes.indexOf(value.selectedNote), false)
+
+        expanded_view_pager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                viewModel.updateSelectedNotes(viewModel.getAppStateStream().value!!.listOfAllNotes[position])
+            }
+        })
+
 
         viewModel.getAppStateStream().observe(viewLifecycleOwner, Observer { appState ->
-
             appState.selectedNote?.let {
-                expandedNoteTxt.text = it.body
                 (activity as NotesActivity).setActionBarTitle(it.head)
-                view!!.background = Utils.getBackGroundRes(context!!, it.bg)
             }
-            oldAppState = appState
         })
-    }
-
-    private fun setDoubleTapListener() {
-        val gestureDetector =
-            GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
-                override fun onDoubleTap(e: MotionEvent): Boolean {
-                    operEditor()
-                    return true
-                }
-            })
-        expandedNoteTxt.setOnTouchListener { _, event ->
-            gestureDetector.onTouchEvent(event)
-        }
     }
 
     override fun onCreateView(
@@ -82,7 +81,7 @@ class ExpandedNoteFragment : Fragment() {
         )
     }
 
-    private fun operEditor() {
+    private fun openEditor() {
         val action =
             ExpandedNoteFragmentDirections.actionExpandedNoteFragmentToUpdateNoteFragment(true)
         findNavController(this).navigateSafe(R.id.expandedNoteFragment, action)
---
+++ app/src/main/java/com/andor/navigate/notepad/expanded/ViewPagerAdapter.kt
@@ -0,0 +1,74 @@
+package com.andor.navigate.notepad.expanded
+
+import android.content.Context
+import android.text.method.ScrollingMovementMethod
+import android.view.*
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.andor.navigate.notepad.R
+import com.andor.navigate.notepad.core.Utils
+import com.andor.navigate.notepad.listing.dao.NoteModel
+import java.util.*
+
+class ViewPagerAdapter(
+    private val context: Context,
+    private val fragCallback: (ViewPageItemEvent) -> Unit
+) :
+    RecyclerView.Adapter<ViewPagerAdapter.ViewPageHolder>() {
+
+    lateinit var pageList: ArrayList<NoteModel>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPageHolder {
+        val itemView =
+            LayoutInflater.from(context).inflate(R.layout.item_viewpage, parent, false)
+        return ViewPageHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = pageList.size
+
+    override fun onBindViewHolder(holder: ViewPageHolder, position: Int) {
+        holder.expandedText.text = pageList[position].body
+        holder.expandedText.movementMethod = ScrollingMovementMethod()
+        holder.itemContainer.background = Utils.getBackGroundRes(context, pageList[position].bg)
+
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    inner class ViewPageHolder(val view: View) : RecyclerView.ViewHolder(view),
+        View.OnTouchListener {
+        val expandedText: TextView = view.findViewById(R.id.expandedNoteTxt)
+        val itemContainer: View = view.findViewById(R.id.viewPageItemContainer)
+
+        init {
+            expandedText.setOnTouchListener(this)
+        }
+
+        private val gestureDetector =
+            GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+                override fun onDoubleTap(e: MotionEvent): Boolean {
+                    fragCallback.invoke(
+                        ViewPageItemEvent.DoubleClickEvent(
+                            adapterPosition
+                        )
+                    )
+                    return true
+                }
+            })
+
+        override fun onTouch(targetView: View?, motionEvent: MotionEvent?): Boolean {
+            gestureDetector.onTouchEvent(motionEvent)
+            return true
+        }
+    }
+}
+
+sealed class ViewPageItemEvent {
+    data class DoubleClickEvent(val adapterPosition: Int) : ViewPageItemEvent()
+}
